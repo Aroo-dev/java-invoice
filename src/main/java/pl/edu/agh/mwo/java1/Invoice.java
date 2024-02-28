7d824dc4f0fb07eebad114b50f1bbe540ba5b6fa
aroo-dev <axxrxxo@gmail.com>
2024-02-28 06:08:28 +0100

    init

---
+++ src/main/java/pl/edu/agh/mwo/java1/Invoice.java
@@ -0,0 +1,54 @@
+package pl.edu.agh.mwo.java1;
+
+import pl.edu.agh.mwo.java1.product.Product;
+
+import java.math.BigDecimal;
+import java.util.*;
+
+
+
+public class Invoice {
+    private List<Product> products = new ArrayList<>();
+    private Map<Product, Integer> quantities = new HashMap<>();
+
+    public void addProduct(Product product) {
+        if (product == null)
+            throw new IllegalArgumentException("Produkt nie może być zerem.");
+        addProduct(product, 1);
+    }
+
+    public void addProduct(Product product, Integer quantity) {
+        if (quantity <= 0) {
+            throw new IllegalArgumentException("Ilość musi być większa niż 0.");
+        }
+        this.products.add(product);
+        this.quantities.put(product, quantity);
+    }
+
+    public BigDecimal getNetPrice() {
+        BigDecimal netPrice = BigDecimal.ZERO;
+        for (Product product : this.products) {
+            Integer quantity = this.quantities.getOrDefault(product, 1);
+            BigDecimal productPrice = product.getPrice().multiply(BigDecimal.valueOf(quantity));
+            netPrice = netPrice.add(productPrice);
+        }
+
+        return netPrice;
+    }
+
+    public BigDecimal getTax() {
+        BigDecimal tax = BigDecimal.ZERO;
+        for (Product product : this.products) {
+            Integer quantity = this.quantities.getOrDefault(product, 1);
+            BigDecimal productTax = product.getPriceWithTax().subtract(product.getPrice()).multiply(BigDecimal.valueOf(quantity));
+            tax = tax.add(productTax);
+        }
+        return tax;
+    }
+
+    public BigDecimal getGrossPrice() {
+        BigDecimal netPrice = getNetPrice();
+        BigDecimal tax = getTax();
+        return netPrice.add(tax);
+    }
+}
---
+++ src/main/java/pl/edu/agh/mwo/java1/Main.java
@@ -0,0 +1,7 @@
+package pl.edu.agh.mwo.java1;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
